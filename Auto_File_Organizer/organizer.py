ee33a0a59780bd72a3255b6d5e9207dbef9f3e8b
Samiksha Bhore <samikshabhore7636@gmail.com>
2026-04-23 11:43:16 +0530

    Upgraded Auto file organizer

--- Auto_File_Organizer/organizer.py
+++ Auto_File_Organizer/organizer.py
@@ -2,27 +2,45 @@ import os
 import shutil
 
 # Folder path 
-folder_path = r"C:\Users\SAMIKSHA BHORE\Downloads"
+folder_path = r"C:\Users\SAMIKSHA BHORE\Documents"
 
-# Get all files
-files = os.listdir(folder_path)
+file_types = {
+    "Images" : ["jpg", 'png', 'jpeg'],
+    "Documents" : ['pdf', 'txt', 'docx'],
+    "Audio": ['mp3', 'wav'],
+    "Videos" : ['mp4', 'mkv']
+}
 
-for file in files:
+
+for file in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file)
+
 
     # Skip folders
     if os.path.isdir(file_path):
         continue
 
     # Get extension
-    ext = file.split(".")[-1]
+    ext = file.split(".")[-1].lower()
 
-    #create folder based on extension
-    dest_folder = os.path.join(folder_path, ext.upper())
+    moved = False
 
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
+    for folder, extensions in file_types.items():
+        if ext in extensions:
+            dest = os.path.join(folder_path, folder)
 
-    # Move file
-    shutil.move(file_path, os.path.join(dest_folder, file))
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+
+            shutil.move(file_path, os.path.join(dest, file))
+            moved = True
+            break    
+
+    if not moved:
+        other = os.path.join(folder_path, 'others')
+        if not os.path.exists(other):
+            os.makedirs(other)
+
+        shutil.move(file_path, os.path.join(other,file))        
+    
 
